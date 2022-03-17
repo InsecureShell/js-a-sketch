60fc3716c64b37088b2046cd952c35f2c832b957
Sasha Mortimer <6824835+Shaefa@users.noreply.github.com>
2022-03-16 22:38:45 -0400

    add block-hover css class and add js code to give the blocks this class when hovering

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ let canvas = document.getElementById("canvas");
 function createCanvas() {
     // loop to create 256 blocks and add them to the canvas
     for(let i = 0; i < 256; i++) {
+        // create a div and give it the "block" class
         let block = document.createElement('div');
         block.classList.add("block");
 
+        // add mouseenter event listener to color the block 
+        // when the mouse hovers over it
+        block.addEventListener("mouseenter", function() {
+            block.classList.add("block-hover");
+        });
+
+        // add the block to the canvas
         canvas.appendChild(block);
         
         console.log(`Created block #${i}`);
