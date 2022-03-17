c4b888d082b6cee560addef3bed1f7d57791cdf3
Sasha Mortimer <6824835+Shaefa@users.noreply.github.com>
2022-03-16 22:19:49 -0400

    add variable for the canvas to reuse later

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let canvas = document.getElementById("canvas");
+
 function createCanvas() {
     // loop to create 256 blocks and add them to the canvas
     for(let i = 0; i < 256; i++) {
         let block = document.createElement('div');
         block.classList.add("block");
 
-        document.getElementById("canvas").appendChild(block);
+        canvas.appendChild(block);
         
         console.log(`Created block #${i}`);
     };
